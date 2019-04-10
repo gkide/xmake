0d446dc305f5ddec766003559d5dff0ad221be88
Charlie WONG <1213charlie@163.com>
2019-04-10 22:09:55 +0800

    docs: local make configuration

---
+++ docs/local.mk
@@ -0,0 +1,18 @@
+# make V=1
+# make O=...
+# make I=...
+
+#BUILD_TYPE ?= Debug
+#BUILD_TYPE ?= Release
+
+#BUILD_CMD ?= ninja
+#GENERATOR ?= 'Ninja'
+
+#BUILD_CMD ?= $(MAKE)
+#GENERATOR ?= 'Unix Makefiles'
+
+#BUILD_DIR ?= $(SOURCE_DIR)/build
+#INSTALL_PREFIX ?= $(BUILD_DIR)/usr
+
+#CMAKE_ARGS += ...
+#EXTRA_CMAKE_ARGS += ...
