891b3801ec992f3e0fffc7b0fa23673402cd7fe1
Charlie WONG <1213charlie@163.com>
2019-04-18 23:54:22 +0800

    fix: tarball downloding & extracting

--- cmake/xmake/DownloadExtract.cmake
+++ cmake/xmake/DownloadExtract.cmake
@@ -3,34 +3,34 @@
 #    EXPECTED_SHA256 ...
 #    DOWNLOAD_TO ...
 #    EXTRACT_TO ...
-#    FORCE)
+#)
 function(DownloadExtract)
-    cmake_parse_arguments(_def
-        "FORCE" # Force download & extract if already done
+    cmake_parse_arguments(tarball
+        ""
         "URL;TARGET;EXPECTED_SHA256;DOWNLOAD_TO;EXTRACT_TO"
         ""
         ${ARGN})
 
-    if(NOT _def_TARGET) # Download/Extract target
+    if(NOT tarball_TARGET) # Download/Extract target
         message(FATAL_ERROR "TARGET must be passed.")
     endif()
 
-    if(NOT _def_URL) # Target download URL
+    if(NOT tarball_URL) # Target download URL
         message(FATAL_ERROR "URL must be passed.")
     endif()
 
-    if(NOT _def_EXPECTED_SHA256) # Expected target SHA256
-        option(SKIP_SHA256_CHECKING "Skip check target's SHA256" ON)
+    if(NOT tarball_EXPECTED_SHA256) # Expected target SHA256
+        set(tarball_skip_sha256_check ON)
     endif()
 
-    set(DOWNLOAD_TO "${DEPS_DOWNLOAD_DIR}/${_def_TARGET}")
-    if(_def_DOWNLOAD_TO) # Target download directory
-        set(DOWNLOAD_TO "${_def_DOWNLOAD_TO}/${_def_TARGET}")
+    set(DOWNLOAD_TO "${DEPS_DOWNLOAD_DIR}/${tarball_TARGET}")
+    if(tarball_DOWNLOAD_TO) # Target download directory
+        set(DOWNLOAD_TO "${tarball_DOWNLOAD_TO}/${tarball_TARGET}")
     endif()
 
-    set(SOURCE_DIR "${DEPS_BUILD_DIR}/${_def_TARGET}")
-    if(_def_EXTRACT_TO) # Target extract directory
-        set(SOURCE_DIR "${_def_EXTRACT_TO}/${_def_TARGET}")
+    set(SOURCE_DIR "${DEPS_BUILD_DIR}/${tarball_TARGET}")
+    if(tarball_EXTRACT_TO) # Target extract directory
+        set(SOURCE_DIR "${tarball_EXTRACT_TO}/${tarball_TARGET}")
     endif()
 
     file(MAKE_DIRECTORY "${DOWNLOAD_TO}")
@@ -39,15 +39,15 @@ function(DownloadExtract)
     # Downloading #
     ###############
     # Parse URL to get file name
-    string(REGEX MATCH "[^/\\?]*$" fname "${_def_URL}")
+    string(REGEX MATCH "[^/\\?]*$" fname "${tarball_URL}")
     if(NOT "${fname}" MATCHES "(\\.|=)(bz2|tar|tgz|tar\\.gz|zip)$")
         string(REGEX MATCH "([^/\\?]+(\\.|=)(bz2|tar|tgz|tar\\.gz|zip))/.*$"
-               match_result "${_def_URL}")
+               match_result "${tarball_URL}")
         set(fname "${CMAKE_MATCH_1}")
     endif()
 
     if(NOT "${fname}" MATCHES "(\\.|=)(bz2|tar|tgz|tar\\.gz|zip)$")
-        message(FATAL_ERROR "Not found tarball filename from URL: ${_def_URL}")
+        message(FATAL_ERROR "Not found tarball filename from URL: ${tarball_URL}")
     endif()
 
     string(REPLACE ";" "-" fname "${fname}")
@@ -55,22 +55,18 @@ function(DownloadExtract)
 
     # If tarball exists, if not force then do nothing
     if(EXISTS "${SRC_TARBALL}")
-        if(_def_FORCE)
-            message(STATUS "Downloading ... skip")
-            message(STATUS "Exists ${SRC_TARBALL}")
-        else()
-            return()
-        endif()
+        message(STATUS "Downloading ... skip")
+        message(STATUS "Exists ${SRC_TARBALL}")
     else()
         set(timeout 60)
         set(timeout_msg "${timeout} seconds")
         set(timeout_arg INACTIVITY_TIMEOUT ${timeout})
         message(STATUS "Downloading ...
    Timeout  = ${timeout_msg}
-   From URL = ${_def_URL}
+   From URL = ${tarball_URL}
    Save As  = ${SRC_TARBALL}")
 
-        file(DOWNLOAD ${_def_URL} ${SRC_TARBALL}
+        file(DOWNLOAD ${tarball_URL} ${SRC_TARBALL}
             ${timeout_arg}
             SHOW_PROGRESS
             STATUS status
@@ -80,19 +76,18 @@ function(DownloadExtract)
         list(GET status 1 status_string)
 
         if(NOT status_code EQUAL 0)
-            message(FATAL_ERROR "Error: failed downloading from ${_def_URL}
+            message(FATAL_ERROR "Error: failed downloading from ${tarball_URL}
 Status Code: ${status_code}
 Status String: ${status_string}
 Log: ${log}")
         endif()
         message(STATUS "Downloading ... done.")
     endif()
-    message(STATUS "")
 
     ############
     # Checking #
     ############
-    if(SKIP_SHA256_CHECKING)
+    if(tarball_skip_sha256_check)
         message(STATUS "Checking SHA256 skip.")
     else()
         message(STATUS "Checking HASH256 ...")
@@ -106,20 +101,19 @@ Log: ${log}")
         if(ACTUAL_SHA256 STREQUAL "${EMPTY_SHA256}")
             # File was empty. It's likely due to lack of SSL support.
             file(REMOVE ${SRC_TARBALL})
-            message(FATAL_ERROR "Failed to download ${_def_URL}.
+            message(FATAL_ERROR "Failed to download ${tarball_URL}.
 The file is empty and likely means CMake was built without SSL support.
 Please use a version of CMake with proper SSL support and try again.")
-        elseif((NOT _def_EXPECTED_SHA256 STREQUAL NULL_SHA256)
-               AND (NOT _def_EXPECTED_SHA256 STREQUAL ACTUAL_SHA256))
+        elseif((NOT tarball_EXPECTED_SHA256 STREQUAL NULL_SHA256)
+               AND (NOT tarball_EXPECTED_SHA256 STREQUAL ACTUAL_SHA256))
             # Was not a NULL SHA256 and did not match
             file(REMOVE ${SRC_TARBALL})
             message(FATAL_ERROR "Checking HASH256 failed, remove it and try again.
 Actual HASH256  : ${ACTUAL_SHA256}
-Expected HASH256: ${_def_EXPECTED_SHA256}")
+Expected HASH256: ${tarball_EXPECTED_SHA256}")
         endif()
         message(STATUS "Checking HASH256 ... done.")
     endif()
-    message(STATUS "")
 
     ##############
     # Extracting #
@@ -127,7 +121,6 @@ Expected HASH256: ${_def_EXPECTED_SHA256}")
     message(STATUS "Extracting ...
    SRC = ${SRC_TARBALL}
    DST = ${SOURCE_DIR}")
-    message(STATUS "")
 
     if(NOT EXISTS "${SRC_TARBALL}")
         message(FATAL_ERROR "Error: extract file does not exist ${SRC_TARBALL}")
@@ -135,10 +128,10 @@ Expected HASH256: ${_def_EXPECTED_SHA256}")
 
     # Prepare a space for extracting
     set(i 123) # For tmp directory suffix
-    while(EXISTS "${SOURCE_DIR}/../ex-${_def_TARGET}-${i}")
+    while(EXISTS "${SOURCE_DIR}/../ex-${tarball_TARGET}-${i}")
         math(EXPR i "${i} + 1")
     endwhile()
-    set(tmp_dir "${SOURCE_DIR}/../ex-${_def_TARGET}-${i}")
+    set(tmp_dir "${SOURCE_DIR}/../ex-${tarball_TARGET}-${i}")
     file(MAKE_DIRECTORY "${tmp_dir}")
 
     # Extract it
