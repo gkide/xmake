9f673f41435965a6f40e8663f1b9df66e96bc830
Charlie WONG <1213charlie@163.com>
2019-05-20 08:04:06 +0800

    fix(win): nsis set NSIS windows build fix
    - set CMAKE_SOURCE_DIR for cpack runtime, it is not set, WHY?
    - set CMAKE_MODULE_PATH for NSIS.template.in to found the xmake one!

--- cmake/ConfigCpack.cmake
+++ cmake/ConfigCpack.cmake
@@ -6,11 +6,15 @@
 # message(WARNING/FATAL_ERROR ...) works, can be used to debug!
 
 if(CPACK_GENERATOR MATCHES "NSIS")
+    # This is for to find 'xmake/NSIS.template.in', if not set then
+    # try to use the cmake orginal module provided one
+    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/xmake")
+
     # For windows start menu link create
     # A pair of value list: targetName => menu/linkName
     set(NSIS_MENU_LINKS "bin/xtest.exe" "xtest")
-    set(NSIS_INSTALLER_LOGO ${CMAKE_SOURCE_DIR}/docs/res/install.ico)
-    set(NSIS_UNINSTALLER_LOGO ${CMAKE_SOURCE_DIR}/docs/res/uninstall.ico)
+    set(NSIS_INSTALLER_LOGO "${CMAKE_SOURCE_DIR}/docs/res/install.ico")
+    set(NSIS_UNINSTALLER_LOGO "${CMAKE_SOURCE_DIR}docs/res/uninstall.ico")
 endif()
 
 if(CPACK_GENERATOR MATCHES "IFW")
